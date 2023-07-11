69ba81d54daf33d3868d64a6a0066848a3b1ec22
Jiaping Chen <jiapingchen17@gmail.com>
2023-07-11 14:21:30 -0700

    created back_end repo and some folders and files in VS code

---
+++ app/__init__.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
+from flask_cors import CORS
+
+db = SQLAlchemy()
+migrate = Migrate()
+load_dotenv()
+
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    # app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    #     "SQLALCHEMY_DATABASE_URI")
+    if test_config is None:
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("RENDER_DATABASE_URI")
+
+    else:
+        app.config["TESTING"] = True
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+        "SQLALCHEMY_DATABASE_URI")
+        
+    # Import models here for Alembic setup
+    # from app.models.ExampleModel import ExampleModel
+    from app.models.board import Board
+    from app.models.card import Card
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # Register Blueprints here
+    # from .routes import example_bp
+    # app.register_blueprint(example_bp)
+    from.routes import board_bp
+    app.register_blueprint(board_bp)
+
+    from.routes import card_bp
+    app.register_blueprint(card_bp)
+
+    CORS(app)
+    return app
