4128a9626690e3adaf4ca8bebdef66dd26443f4f
Jiaping Chen <jiapingchen17@gmail.com>
2023-07-11 23:06:54 -0700

    created PUT, GET, DELETE routes

--- app/routes.py
+++ app/routes.py
@@ -26,3 +26,51 @@ def create_user():
                     "tennis_level":new_user.tennis_level,
                     "zip_code":new_user.zip_code
                     }}), 201
+# get a user info
+@user_bp.route("/<user_id>", methods = ["GET"])
+def get_one_user(user_id):
+    users = validate_user(User,user_id)
+    if 
+
+
+# update user 
+@user_bp.route("/<user_id", methods = ["PUT"])
+def update_user(user_id):
+    users = validate_user(User,user_id)
+    request_data = request.get_json()
+
+    users.name = request_data["name"]
+    users.tennis_level = request_data["tennis_level"]
+    users.zip_code = request_data["zip_code"]
+
+    db.seesion.commit()
+
+    return jsonify({"user":{
+                    "id":new_user.user_id,
+                    "name":new_user.name,
+                    "tennis_level":new_user.tennis_level,
+                    "zip_code":new_user.zip_code
+    }}), 200
+
+# delete user
+@user_bp.route("/<user_id>", methods = ["DELETE"])
+def delete_user(user_id):
+    users = validate_user(User, user_id)
+
+    db.seesion.delete(users)
+    db.session.commit()
+
+    return {"details": f'User {user_id} deleted successfully!'}
+
+
+# helper funtion (validate_user)
+def validate_user(model, user_id):
+    try:
+        user_id = int(user_id)
+    except ValueError:
+        abort(make_response({"mes":"id is invalide input"}, 400))
+    
+    user = model.query.get(user_id)
+    if user is None:
+        abort(make_response({"msg": "User not found"}, 404))
+    return user 
