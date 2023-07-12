9087e301c042731fb53129208006a51ea9ca8688
Laura <lauraemiliaperez@outlook.com>
2023-07-12 14:38:44 -0700

    patch route with changes

--- app/routes.py
+++ app/routes.py
@@ -56,21 +56,22 @@ def update_user(user_id):
     user = validate_user(User,user_id)
     request_data = request.get_json()
     
-    if request_data.get("preferences"):
-        user.preferences = request_data["preferences"]
-    elif request_data.get("name"):
-        user.name = request_data["name"]
-    elif request_data.get("tennis_level"):
-        user.tennis_level = request_data["tennis_level"]
-    elif request_data.get("zip_code"):
-        user.zip_code = request_data["zip_code"]
-    elif request_data.get("email"):
-        user.email = request_data["email"]
+    update_user = request_data
+    # if request_data.get("preferences"):
+    #     user.preferences = request_data["preferences"]
+    # elif request_data.get("name"):
+    #     user.name = request_data["name"]
+    # elif request_data.get("tennis_level"):
+    #     user.tennis_level = request_data["tennis_level"]
+    # elif request_data.get("zip_code"):
+    #     user.zip_code = request_data["zip_code"]
+    # elif request_data.get("email"):
+    #     user.email = request_data["email"]
     
-
+    
     db.session.commit()
-
-    return {"user":user.to_dict()}, 200
+    return jsonify(update_user), 200
+    # return {"user":user.to_dict()}, 200
 
 
 # delete user
